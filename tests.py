07c62fdaf3d71de1085086ce42ba876a1346fb47
Nast8065 <n9069968806n@yandex.ru>
2025-08-05 15:07:15 +0700

    Update tests.py
    I removed the unnecessary check and added a new one, prescribed the use of the collector fixture

--- tests.py
+++ tests.py
@@ -24,8 +24,7 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
 	#проверяем, что книге можно установить жанр
-    def test_set_book_genre_one_book_can_set_the_genre(self):
-        collector = BooksCollector()
+    def test_set_book_genre_one_book_can_set_the_genre(self, collector):
         collector.add_new_book('Ходячие мертвецы')
         collector.set_book_genre('Ходячие мертвецы', 'Ужасы')
         assert collector.books_genre['Ходячие мертвецы'] == 'Ужасы'
@@ -37,8 +36,7 @@ class TestBooksCollector:
         assert collector.get_book_genre('Шерлок') == 'Детективы'
 
         #проверяем, вывод списка книг с определенным жанром
-    def test_get_books_with_specific_genre_two_horror_book(self):
-        collector = BooksCollector()
+    def test_get_books_with_specific_genre_two_horror_book(self, collector):
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Фантастика')
         collector.add_new_book('Ходячие мертвецы')
@@ -49,8 +47,7 @@ class TestBooksCollector:
         assert result == ['Ходячие мертвецы', 'Гордость и предубеждение и зомби']
 
         #проверяем, что выводится словарь books_genre
-    def test_get_books_genre_of_three_books(self):
-        collector = BooksCollector()
+    def test_get_books_genre_of_three_books(self, collector):
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Фантастика')
         collector.add_new_book('Ходячие мертвецы')
@@ -60,8 +57,7 @@ class TestBooksCollector:
         assert collector.get_books_genre() == {'Что делать, если ваш кот хочет вас убить' : 'Фантастика', 'Ходячие мертвецы' : 'Ужасы', 'Гордость и предубеждение и зомби' : 'Ужасы'} 
 
         #проверяем, что выводятся книги, подходящие детям
-    def test_get_books_for_children_one_book_display_cartoons(self):
-        collector = BooksCollector()
+    def test_get_books_for_children_one_book_display_cartoons(self, collector):
         collector.add_new_book('Маша и медведь')
         collector.set_book_genre('Маша и медведь', 'Мультфильмы')
         collector.add_new_book('Ходячие мертвецы')
@@ -69,16 +65,14 @@ class TestBooksCollector:
         assert collector.get_books_for_children() == ['Маша и медведь']
 
         #проверяем, что книга добавляется в избранное
-    def test_add_book_in_favorites_one_book_add_to_favorite(self):
-        collector = BooksCollector()
+    def test_add_book_in_favorites_one_book_add_to_favorite(self, collector):
         collector.add_new_book('Маша и медведь')
         collector.add_new_book('Ходячие мертвецы')
         collector.add_book_in_favorites('Ходячие мертвецы')
         assert 'Ходячие мертвецы' in collector.favorites
 
         #проверяем, что книгу можно удалить из избранного
-    def test_delete_book_from_favorites_was_deleted_one_book(self):
-        collector = BooksCollector()
+    def test_delete_book_from_favorites_was_deleted_one_book(self, collector):
         collector.add_new_book('Маша и медведь')
         collector.add_new_book('Ходячие мертвецы')
         collector.add_book_in_favorites('Ходячие мертвецы')
@@ -86,8 +80,7 @@ class TestBooksCollector:
         assert len(collector.favorites) == 0
 
         #проверяем, вывод списка избранных книг
-    def test_get_list_of_favorites_books_got_list(self):
-        collector = BooksCollector()
+    def test_get_list_of_favorites_books_got_list(self, collector):
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.add_new_book('Ходячие мертвецы')
         collector.add_new_book('Маша и медведь')
@@ -102,8 +95,7 @@ class TestBooksCollector:
         assert len(collector.books_genre) == 1
 	
 	#проверяем, что одну и ту же книгу можно добавить только один раз
-    def test_add_new_book_Identical_books_can_only_be_added_once(self):
-        collector = BooksCollector()
+    def test_add_new_book_Identical_books_can_only_be_added_once(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Гордость и предубеждение и зомби')
         assert len(collector.get_books_rating()) == 1
